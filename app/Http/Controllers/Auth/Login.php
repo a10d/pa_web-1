bd11a132f9f7254ed89af6be69134245d7e60d68
Aaron Schmied <aaron@schmied.dev>
2023-10-21 16:01:41 +0200

    wip

--- app/Http/Controllers/Auth/Login.php
+++ app/Http/Controllers/Auth/Login.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Foundation\Http\Controller;
 use app\Http\Requests\Auth\Login as LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class Login extends Controller
 {
-    public function __invoke(LoginRequest $request)
+    public function __invoke(LoginRequest $request): JsonResponse
     {
         Auth::attempt($request->only('email', 'password'));
 
--- app/Http/Controllers/Task/DeleteTask.php
+++ app/Http/Controllers/Task/DeleteTask.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Task;
 use App\Foundation\Http\Controller;
 use App\Models\Task;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
 use Throwable;
 
 class DeleteTask extends Controller
