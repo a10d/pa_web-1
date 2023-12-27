3d391e4fb93ff0a8b67a6e8d5b302b3f302c5c71
Aaron Schmied <aaron@schmied.dev>
2023-12-27 19:37:16 +0100

    Added cors handling

--- backend_go/api.go
+++ backend_go/api.go
@@ -5,6 +5,7 @@ import (
     "errors"
     "fmt"
 	"github.com/gorilla/mux"
+    "github.com/rs/cors"
     "github.com/thedevsaddam/govalidator"
 	"log"
 	"net/http"
@@ -29,30 +30,42 @@ func NewAPIServer(listenAddr string, store Storage) *APIServer {
 func (s *APIServer) Run() error {
     s.registerRoutes()
     s.registerCustomValidationRules()
+    return s.listenAndServe()
+}
+
+func (s *APIServer) listenAndServe() error {
 
 	log.Println("Listening on", s.listenAddr)
 
-    return http.ListenAndServe(s.listenAddr, s.router)
+    c := cors.New(cors.Options{
+        AllowCredentials: true,
+        AllowedHeaders:   []string{"*"},
+        AllowedOrigins:   []string{"*"},
+        AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+    })
+
+    return http.ListenAndServe(s.listenAddr, c.Handler(s.router))
 }
 
 func (s *APIServer) registerRoutes() {
+
     s.router.HandleFunc("/todos", makeServeHandleFunc(s.handleGetTodos)).Methods(http.MethodGet)
     s.router.HandleFunc("/todos", makeServeHandleFunc(s.handleCreateTodo)).Methods(http.MethodPost)
-    s.router.HandleFunc("/todos/{id}", makeServeHandleFunc(s.handleGetTodoById)).Methods(http.MethodGet)
-    s.router.HandleFunc("/todos/{id}", makeServeHandleFunc(s.handleUpdateTodo)).Methods(http.MethodPut)
-    s.router.HandleFunc("/todos/{id}", makeServeHandleFunc(s.handleDeleteTodo)).Methods(http.MethodDelete)
+    s.router.HandleFunc("/todo/{id}", makeServeHandleFunc(s.handleGetTodoById)).Methods(http.MethodGet)
+    s.router.HandleFunc("/todo/{id}", makeServeHandleFunc(s.handleUpdateTodo)).Methods(http.MethodPut)
+    s.router.HandleFunc("/todo/{id}", makeServeHandleFunc(s.handleDeleteTodo)).Methods(http.MethodDelete)
 
     s.router.HandleFunc("/todoTypes", makeServeHandleFunc(s.handleGetTodoTypes)).Methods(http.MethodGet)
     s.router.HandleFunc("/todoTypes", makeServeHandleFunc(s.handleCreateTodoType)).Methods(http.MethodPost)
-    s.router.HandleFunc("/todoTypes/{id}", makeServeHandleFunc(s.handleGetTodoTypeById)).Methods(http.MethodGet)
-    s.router.HandleFunc("/todoTypes/{id}", makeServeHandleFunc(s.handleUpdateTodoType)).Methods(http.MethodPut)
-    s.router.HandleFunc("/todoTypes/{id}", makeServeHandleFunc(s.handleDeleteTodoType)).Methods(http.MethodDelete)
+    s.router.HandleFunc("/todoType/{id}", makeServeHandleFunc(s.handleGetTodoTypeById)).Methods(http.MethodGet)
+    s.router.HandleFunc("/todoType/{id}", makeServeHandleFunc(s.handleUpdateTodoType)).Methods(http.MethodPut)
+    s.router.HandleFunc("/todoType/{id}", makeServeHandleFunc(s.handleDeleteTodoType)).Methods(http.MethodDelete)
 
     s.router.HandleFunc("/users", makeServeHandleFunc(s.handleGetUsers)).Methods(http.MethodGet)
     s.router.HandleFunc("/users", makeServeHandleFunc(s.handleCreateUser)).Methods(http.MethodPost)
-    s.router.HandleFunc("/users/{id}", makeServeHandleFunc(s.handleGetUserById)).Methods(http.MethodGet)
-    s.router.HandleFunc("/users/{id}", makeServeHandleFunc(s.handleUpdateUser)).Methods(http.MethodPut)
-    s.router.HandleFunc("/users/{id}", makeServeHandleFunc(s.handleDeleteUser)).Methods(http.MethodDelete)
+    s.router.HandleFunc("/user/{id}", makeServeHandleFunc(s.handleGetUserById)).Methods(http.MethodGet)
+    s.router.HandleFunc("/user/{id}", makeServeHandleFunc(s.handleUpdateUser)).Methods(http.MethodPut)
+    s.router.HandleFunc("/user/{id}", makeServeHandleFunc(s.handleDeleteUser)).Methods(http.MethodDelete)
 
     s.router.HandleFunc("/import", makeServeHandleFunc(s.handleImportData)).Methods(http.MethodPost)
     s.router.HandleFunc("/export", makeServeHandleFunc(s.handleExportData)).Methods(http.MethodGet)
@@ -169,12 +182,16 @@ type apiError struct {
 
 func makeServeHandleFunc(f apiFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
+
         t := time.Now().Format(time.TimeOnly)
         fmt.Printf("%s | \033[0;31m%s\033[0m  \t| %s\n", t, r.Method, r.RequestURI)
-		if err := f(w, r); err != nil {
+
+        if err := f(w, r); err != nil {
+
             fmt.Printf("%s | \033[0;31m\t\t  err: %s\u001B[0m\n", t, err.Error())
 			err := writeJSON(w, http.StatusInternalServerError, apiError{Error: err.Error()})
-			if err != nil {
+
+            if err != nil {
 				return
 			}
 		}
