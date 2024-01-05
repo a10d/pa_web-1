291b0cf780a7796b6eacf560e01cb91cf1ca864c
Aaron Schmied <aaron@schmied.dev>
2024-01-05 11:47:54 +0100

    Removed unused imports

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import("tailwindcss").Config} */
 export default {
     content: [
-        "./src/**/*.blade.php",
         "./src/**/*.ts",
         "./src/**/*.vue",
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                'handwriting': ['"Architects Daughter"', 'handwriting'],
-            }
-        },
+        extend: {},
     },
     plugins: [],
 };
